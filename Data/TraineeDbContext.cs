fbf5e2c8ca0413d9852c025831b10d4128593694
ArchanaABillava <archanaabillava0154@gmail.com>
2022-10-16 09:59:11 +0530

    Add project files.

---
+++ Data/TraineeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TE_CODEFIRST.Models;
+//using NToastNotify;
+
+namespace TE_CODEFIRST.Data
+{
+    public class TraineeDbContext: DbContext
+    {
+        public TraineeDbContext(DbContextOptions<TraineeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Trainee> Trainees { get; set; }
+
+    }
+}
---
+++ Migrations/20221014123109_initial_1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TE_CODEFIRST.Migrations
+{
+    public partial class initial_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
